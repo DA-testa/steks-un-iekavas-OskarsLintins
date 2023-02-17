2f1a5169b56f5d70e15084f57b2d86ed1857bcef
OskarsLintins <124681718+OskarsLintins@users.noreply.github.com>
2023-02-17 11:31:42 +0000

    Oskars Lintins 221RDB434

--- main.py
+++ main.py
@@ -29,15 +29,20 @@ def main():
     text = input()
     if text == "I":
         text = input()
+        mismatch = find_mismatch(text)
+        if mismatch: 
+            print(mismatch)
+        else:
+            print("Success")
     elif text == "F":
-        text = input()
-        with open('/workspaces/steks-un-iekavas-OskarsLintins/test/' + text) as file:
-            text = file.read()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-    if mismatch: 
-        print(mismatch)
-    else:
-        print("Success")
+        for i in range(0,6):
+            text = str(i)
+            with open('/workspaces/steks-un-iekavas-OskarsLintins/test/' + text) as file:
+                text = file.read()
+                mismatch = find_mismatch(text)
+                if mismatch: 
+                    print(mismatch)
+                else:
+                    print("Success")
 if __name__ == "__main__":
     main()
